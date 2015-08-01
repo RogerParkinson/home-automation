b1d6324a5cfad6c6304af051cb8411b71e7667a4
rogerparkinson <roger@senanque.co.nz>
2015-08-01 21:33:29 +1200

    cleaned up binary files

--- RFReporter/Debug/src/I2CHandler.d
+++
@@ -1,3 +0,0 @@
-src/I2CHandler.d: ../src/I2CHandler.cpp ../src/I2CHandler.h
-
-../src/I2CHandler.h:
--- RFReporter/Debug/src/RFReporter.d
+++
@@ -1,6 +0,0 @@
-src/RFReporter.d: ../src/RFReporter.cpp ../src/I2CHandler.h \
- ../src/Logger.h
-
-../src/I2CHandler.h:
-
-../src/Logger.h:
